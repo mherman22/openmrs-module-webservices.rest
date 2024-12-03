a3de50016b72e967979c17e94ee2025aab1c6957
mherman22 <hermanmuhereza22@gmail.com>
2024-12-03 14:04:38 +0300

    RESTWS-963: Add a swagger autogeneration util to scan resource handlers and generate the swagger spec

--- omod-common/src/main/java/org/openmrs/module/webservices/docs/swagger/SwaggerSpecificationCreator.java
+++ omod-common/src/main/java/org/openmrs/module/webservices/docs/swagger/SwaggerSpecificationCreator.java
@@ -77,6 +77,8 @@ public class SwaggerSpecificationCreator {
 
 	private static Swagger swagger;
 
+	private static String cachedJson;
+
 	private String host;
 
 	private String basePath;
@@ -146,14 +148,15 @@ public class SwaggerSpecificationCreator {
 	}
 
 	public String getJSON() {
-		if (isCached()) {
+		if (isCached() && cachedJson != null) {
 			log.info("Returning a cached copy of Swagger specification");
-			initSwagger();
-		} else {
-			swagger = new Swagger();
-			BuildJSON();
+			return cachedJson;
 		}
-		return createJSON();
+
+		swagger = new Swagger();
+		BuildJSON();
+		cachedJson = createJSON();
+		return cachedJson;
 	}
 
 	private void addDefaultDefinitions() {
@@ -831,8 +834,8 @@ public class SwaggerSpecificationCreator {
 
 			Map<String, Property> properties = definition.getProperties();
 
-			// 2. merge subclass properties into definition
-			for (Map.Entry<String, Property> prop : resourceHandler.getGETModel(Representation.FULL).getProperties()
+//			 2. merge subclass properties into definition
+			for (Map.Entry<String, Property> prop : SwaggerGenerationUtil.generateGETModel(resourceHandler, Representation.FULL).getProperties()
 					.entrySet()) {
 				if (properties.get(prop.getKey()) == null) {
 					properties.put(prop.getKey(), prop.getValue());
@@ -842,36 +845,6 @@ public class SwaggerSpecificationCreator {
 			// 3. update description
 			post.setDescription("Certain properties may be required depending on type");
 		}
-	}
-
-	@Deprecated
-	private List<org.openmrs.module.webservices.docs.swagger.Parameter> getParametersListForSearchHandlers(
-			String resourceName, String searchHandlerId, int queryIndex) {
-		List<org.openmrs.module.webservices.docs.swagger.Parameter> parameters = new ArrayList<org.openmrs.module.webservices.docs.swagger.Parameter>();
-		String resourceURL = getResourceUrl(getBaseUrl(), resourceName);
-		for (SearchHandlerDoc searchDoc : searchHandlerDocs) {
-			if (searchDoc.getSearchHandlerId().equals(searchHandlerId) && searchDoc.getResourceURL().equals(resourceURL)) {
-				SearchQueryDoc queryDoc = searchDoc.getSearchQueriesDoc().get(queryIndex);
-				for (SearchParameter requiredParameter : queryDoc.getRequiredParameters()) {
-					org.openmrs.module.webservices.docs.swagger.Parameter parameter = new org.openmrs.module.webservices.docs.swagger.Parameter();
-					parameter.setName(requiredParameter.getName());
-					parameter.setIn("query");
-					parameter.setDescription("");
-					parameter.setRequired(true);
-					parameters.add(parameter);
-				}
-				for (SearchParameter optionalParameter : queryDoc.getOptionalParameters()) {
-					org.openmrs.module.webservices.docs.swagger.Parameter parameter = new org.openmrs.module.webservices.docs.swagger.Parameter();
-					parameter.setName(optionalParameter.getName());
-					parameter.setIn("query");
-					parameter.setDescription("");
-					parameter.setRequired(false);
-					parameters.add(parameter);
-				}
-				break;
-			}
-		}
-		return parameters;
 	}
 
 	private String createJSON() {
@@ -999,14 +972,14 @@ public class SwaggerSpecificationCreator {
 		Model modelFull = null;
 
 		if (definitionName.endsWith("Get")) {
-			model = resourceHandler.getGETModel(Representation.DEFAULT);
-			modelRef = resourceHandler.getGETModel(Representation.REF);
-			modelFull = resourceHandler.getGETModel(Representation.FULL);
+			model = SwaggerGenerationUtil.generateGETModel(resourceHandler, Representation.DEFAULT);
+			modelRef = SwaggerGenerationUtil.generateGETModel(resourceHandler, Representation.REF);
+			modelFull = SwaggerGenerationUtil.generateGETModel(resourceHandler, Representation.FULL);
 		} else if (definitionName.endsWith("Create")) {
-			model = resourceHandler.getCREATEModel(Representation.DEFAULT);
-			modelFull = resourceHandler.getCREATEModel(Representation.FULL);
+			model = SwaggerGenerationUtil.generateCREATEModel(resourceHandler, Representation.DEFAULT);
+			modelFull = SwaggerGenerationUtil.generateCREATEModel(resourceHandler, Representation.FULL);
 		} else if (definitionName.endsWith("Update")) {
-			model = resourceHandler.getUPDATEModel(Representation.DEFAULT);
+			model = SwaggerGenerationUtil.generateUPDATEModel(resourceHandler, Representation.DEFAULT);
 		}
 
 		if (model != null) {
@@ -1025,7 +998,6 @@ public class SwaggerSpecificationCreator {
 	 * @param operationName get, post, delete
 	 * @param resourceName
 	 * @param resourceParentName
-	 * @param representation
 	 * @param operationEnum
 	 * @return
 	 */
@@ -1230,11 +1202,12 @@ public class SwaggerSpecificationCreator {
 	 * @return true if and only if swagger is not null, and its paths are also set.
 	 */
 	public static boolean isCached() {
-		return swagger != null && swagger.getPaths() != null;
+		return swagger != null && swagger.getPaths() != null && !swagger.getPaths().isEmpty();
 	}
 
 	public static void clearCache() {
 		swagger = null;
+		cachedJson = null;
 	}
 
 }
